0e3ff734be8ea5c01d928e400affa23eb169cbf6
bgkharba <bgkharba>
2020-11-05 17:53:15 -0500

    init

---
+++ SSWebApp/src/main/java/com/SunStrider/SSWebApp/SsWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.SunStrider.SSWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsWebAppApplication.class, args);
+	}
+
+}
---
+++ SSWebApp/src/test/java/com/SunStrider/SSWebApp/SsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SunStrider.SSWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
