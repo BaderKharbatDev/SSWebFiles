0e36a5b0619047339657f7cd4ac7bbcf2540bd59
bgkharba <bgkharba>
2020-12-10 18:54:23 -0500

    Saving Kickbotic Files

---
+++ src/main/java/kickboticgenerator/Strike.java
@@ -0,0 +1,16 @@
+package kickboticgenerator;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Strike {
+	@JsonProperty("name")
+	public String name;
+	public StrikeCategory type;
+	public StrikeDifficulty difficulty;
+	public StrikeDistance distance;
+	public StrikeRotation rotation;
+	public Boolean isActive;
+	public Boolean canRepeat;
+	public 
+	
+}
